f1f8088126a3a5e430d67f868c80aa9f06b65e6b
Harshawardhan-max <harshawardhangbirari0209@gmail.com>
2026-04-05 19:28:31 +0530

    Add database connection utility using mongoose

---
+++ server/src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import { env } from './env.js';
+
+export const connectDB = async () => {
+  if (!env.mongoUri) {
+    throw new Error('MONGO_URI is required');
+  }
+
+  await mongoose.connect(env.mongoUri);
+  console.log('MongoDB connected');
+};
